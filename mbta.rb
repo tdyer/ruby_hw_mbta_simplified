45f456d2e2aff153168b579a665072de62e4550d
lpond22 <laura@thelevelup.com>
2016-02-07 08:00:06 -0500

    Added all stations as an array

---
+++ mbta.rb
@@ -0,0 +1,5 @@
+red_line = ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']
+
+green_line = ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley']
+
+orange_line = ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
