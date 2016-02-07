0cb22c2a84e4522ae71aa8d15109b490c2ab6944
lpond22 <laura@thelevelup.com>
2016-02-07 08:40:51 -0500

    Added origin stops on correct line when prompted

--- mbta.rb
+++ mbta.rb
@@ -5,16 +5,47 @@ green_line = ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arli
 orange_line = ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
 
 puts "Welcome to the MBTA!"
-print "What line do you want to start on? red | orange | green"
-origin_line = gets.chomp!.to_s
+puts "What line do you want to start on? red | orange | green: "
+origin_line = gets.chomp!.downcase.to_s
 
-print "What stop are you getting on at?"
-origin_stop = gets.chomp!.to_s
+while origin_line == 'red'
+  puts  "What stop are you getting on at?
+  * Alewife
+  * Davis
+  * Porter
+  * Harvard
+  * Central
+  * Kendall/MIT
+  * Park Street
+  * South Station"
+  origin_stop = gets.chomp!.downcase.to_s
+  while origin_line == 'orange'
+    puts "What stop are you getting on at?
+  * North Station
+  * Haymarket
+  * Park Street
+  * State Street
+  * Downtown Crossing
+  * Chinatown
+  * Tufts Medical Center"
+  origin_stop = gets.chomp!.downcase.to_s
+    while origin_line == 'green'
+      puts "What stop are you getting on at?
+  * Haymarket
+  * Government Center
+  * Park Street
+  * Boylston
+  * Arlington
+  * Copley"
+  origin_stop = gets.chomp!.downcase.to_s
+    end
+  end
+end
 
-print "What line do you want to end on? red | orange | green"
+puts "What line do you want to end on? red | orange | green: "
 destination_line = gets.chomp!.to_s
 
-print "What stop are you getting off at?"
+puts "What stop are you getting off at?: "
 destination_stop = gets.chomp!.to_s
 
 puts origin_line
