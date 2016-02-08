d88f8bfb1d76e09f214253063ce3bfd75f9a8975
dmill <derekm228@gmail.com>
2016-02-08 18:42:43 -0500

    Changed the data structure

--- subway.rb
+++ subway.rb
@@ -2,120 +2,21 @@
 ############################## Define T System ##############################
 #############################################################################
 
-# Represent the entire MBTA with an array of hashes
-# Properties include name, location on their given line, and line name
-mbtaLines = ['red', 'green', 'orange']
-mbtaStations = [
-   {
-      name: "alewife",
-      location: 0,
-      line: "red"
-   },
-   {
-      name: "davis",
-      location: 1,
-      line: "red"
-   },
-   {
-      name: "porter",
-      location: 2,
-      line: "red"
-   },
-   {
-      name: "harvard",
-      location: 3,
-      line: "red"
-   },
-   {
-      name: "central",
-      location: 4,
-      line: "red"
-   },
-   {
-      name: "kendall/mit",
-      location: 5,
-      line: "red"
-   },
-   {
-      name: "park street",
-      location: 6,
-      line: "red"
-   },
-   {
-      name: "south station",
-      location: 7,
-      line: "red"
-   },
-   {
-      name: "haymarket",
-      location: 0,
-      line: "green"
-   },
-   {
-      name: "government center",
-      location: 1,
-      line: "green"
-   },
-   {
-      name: "park street",
-      location: 2,
-      line: "green"
-   },
-   {
-      name: "boylston",
-      location: 3,
-      line: "green"
-   },
-   {
-      name: "arlington",
-      location: 4,
-      line: "green"
-   },
-   {
-      name: "copley",
-      location: 5,
-      line: "green"
-   },
-   {
-      name: "north station",
-      location: 0,
-      line: "orange"
-   },
-   {
-      name: "haymarket",
-      location: 1,
-      line: "orange"
-   },
-   {
-      name: "park street",
-      location: 2,
-      line: "orange"
-   },
-   {
-      name: "state street",
-      location: 3,
-      line: "orange"
-   },
-   {
-      name: "downtown crossing",
-      location: 4,
-      line: "orange"
-   },
-   {
-      name: "chinatown",
-      location: 5,
-      line: "orange"
-   },
-   {
-      name: "tufts medical center",
-      location: 6,
-      line: "orange"
-   }
-]
+# Populate the lines
+redLine = ["alewife", "davis", "porter", "harvard", "central", "kendall/mit", "park street", "south station"]
+greenLine = ["haymarket", "government center", "park street", "boylston", "arlington", "copley"]
+orangeLine = ["north station", "haymarket", "park street", "state street", "downtown crossing", "chinatown", "tufts medical center"]
+
+# Populate the hash
+mbta = {}
+mbta["red"] = redLine
+mbta["green"] = greenLine
+mbta["orange"] = orangeLine
 
 ##############################################################################
-############################# Start the program ##############################
+############################### Get user input ###############################
 ##############################################################################
+
 askingUser = true
 
 while askingUser
@@ -125,8 +26,8 @@ while askingUser
    originLine = gets.chomp
    originLine.downcase!
 
-   #Prevent invalid guess input
-   while !mbtaLines.include?(originLine)
+   # Prevent invalid guess input
+   while !mbta.has_key?(originLine)
       print "\nThat line does not exist. Please enter red, green, or orange: "
       originLine = gets.chomp
       originLine.downcase!
@@ -138,7 +39,9 @@ while askingUser
    originStation.downcase!
 
    #Prevent invalid guess input
-   while !mbtaStations.any? { |station| station[:name] == originStation && station[:line] == originLine}
+   originLine.to_sym
+   originArray = mbta[originLine]
+   while !originArray.include?(originStation)
       print "\nThat station doesn't exist on this line. Please enter a station: "
       originStation = gets.chomp
       originStation.downcase!
@@ -150,7 +53,7 @@ while askingUser
    destinationLine.downcase!
 
    #Prevent invalid guess input
-   while !mbtaLines.include?(destinationLine)
+   while !mbta.has_key?(destinationLine)
       print "\nThat line does not exist. Please enter red, green, or orange: "
       destinationLine = gets.chomp
       destinationLine.downcase!
@@ -162,8 +65,15 @@ while askingUser
    destinationStation.downcase!
 
    #Prevent invalid guess input
-   while !mbtaStations.any? { |station| station[:name] == destinationStation && station[:line] == destinationLine}
+   destinationLine.to_sym
+   destinationArray = mbta[destinationLine]
+   while !destinationArray.include?(destinationStation)
       print "\nThat station doesn't exist on this line. Please enter a station: "
       destinationStation = gets.chomp
       destinationStation.downcase!
    end
+end
+
+##############################################################################
+############################ Calculate the route #############################
+##############################################################################
