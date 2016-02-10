0cad75c07eebdf20014d70b9977bec8f72c34b67
lpond22 <laura@thelevelup.com>
2016-02-09 20:47:21 -0500

    modified Array to be lower case

--- mbta.rb
+++ mbta.rb
@@ -1,9 +1,9 @@
 #populate the MBTA lines
-red_line = ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "South Station"]
+red_line = ["alewife", "davis", "porter", "harvard", "central", "kendall/mit", "park street", "south station"]
 
-green_line = ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"]
+green_line = ["haymarket", "government center", "park street", "boylston", "arlington", "copley"]
 
-orange_line = ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
+orange_line = ["north station", "haymarket", "park street", "state street", "downtown crossing", "chinatown", "tufts medical center"]
 
 # populate the hash
 mbta = {}
@@ -14,37 +14,32 @@ mbta["orange"] = orange_line
 # Get the user's origin line
 puts "Welcome to the MBTA!"
 print "What line do you want to start on? red | orange | green: "
-origin_line = gets.chomp!.downcase
+origin_line = gets.chomp!.downcase!
 
 
 # Get the user's origin station
 # Simplified station selector
 print "What station do you want to get on on the #{origin_line} line? "
-origin_stop = gets.chomp!.downcase
-origin_line.to_sym
+origin_stop = gets.chomp!.downcase!
 originArray = mbta[origin_line]
 
 #Get the user's destination station
 print "What line do you want to end on? red | orange | green: "
-destination_line = gets.chomp!.downcase
+destination_line = gets.chomp!.downcase!
 
 
 # Get the users' destination stop
 print "What station do you want to get on on the #{destination_line} line? "
-destination_stop = gets.chomp!.downcase
-
-destination_line.to_sym
+destination_stop = gets.chomp!.downcase!
 destinationArray = mbta[destination_line]
-#puts "This is your itinerary:"
-#puts "You're taking the #{origin_line} line from #{origin_stop} to #{destination_stop} on the #{destination_line} line."
 
 # Figuring out the distance
 # if same line, count distance between stops
 
 if origin_line == destination_line
-  count_stops = (originArray.index(origin_stop) - destinationArray.index(destination_stop)).abs
-  #puts "You have to go #{count_stops} stops on the #{origin_line}"
-else count_stops = (originArray.index(origin_stop) - originArray.index("Park Street"))
-  count_stops += (destinationArray.index(destination_stop) - destinationArray.index("Park Street")).abs
+   count_stops = (originArray.index(origin_stop) - destinationArray.index(destination_stop)).abs
+else
+   distance = (originArray.index(origin_stop) - originArray.index("Park Street")).abs
+   distance += (destinationArray.index(destination_stop) - destinationArray.index("Park Street")).abs
 end
 puts "To go from #{origin_stop} on the #{origin_line} line to #{destination_stop} on the #{destination_line} line will take #{count_stops}"
