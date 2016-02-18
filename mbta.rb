8da0f05b6cdf807e766d9c9b8696378fc4973c28
lpond22 <laura@thelevelup.com>
2016-02-17 19:48:36 -0500

    Updated descriptions

--- mbta.rb
+++ mbta.rb
@@ -7,6 +7,8 @@ orange_line = ["north station", "haymarket", "park street", "state street", "dow
 
 # populate the hash
 mbta = {}
+# When the response for origin_line equals one of those strings, we'll pull the correct MBTA line.
+# On the Hash MBTA, for key value "red"|"green"|"orange", bring in the above corresponding array into the hash.
 mbta["red"] = red_line
 mbta["green"] = green_line
 mbta["orange"] = orange_line
@@ -14,27 +16,25 @@ mbta["orange"] = orange_line
 # Get the user's origin line
 puts "Welcome to the MBTA!"
 print "What line do you want to start on? red | orange | green: "
-origin_line = gets.chomp!.downcase!
-
+origin_line = gets.chomp!.downcase!.to_s
 
 # Get the user's origin station
 # Simplified station selector
 print "What station do you want to get on on the #{origin_line} line? "
 origin_stop = gets.chomp!.downcase!
 
-origin_line.to_s
+# Creates variable originArray, and grabs the hash corelating to the line you selected in origin_line
 originArray = mbta[origin_line]
 
 # Get the user's destination station
 print "What line do you want to end on? red | orange | green: "
-destination_line = gets.chomp!.downcase!
-
+destination_line = gets.chomp!.downcase!.to_s
 
 # Get the users' destination stop
 print "What station do you want to get on on the #{destination_line} line? "
 destination_stop = gets.chomp!.downcase!
 
-destination_line.to_s
+# Creates variable destinationArray, and grabs the hash corelating to the line you selected in destination_line
 destinationArray = mbta[destination_line]
 
 # Figuring out the distance
